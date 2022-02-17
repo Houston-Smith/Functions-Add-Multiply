ff8761a9553aefd2a916b26bd4f83c67dea1a865
Houston-Smith <houstonbsmith@gmail.com>
2022-02-17 13:04:50 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const add = (num1, num2) => {
+  result = num1 + num2
+  return result
+}
+
+const multiply = (num) => {
+  result = num * 2
+  return result
+}
+
+const number1 = add(5, 5)
+const number2 = multiply(number1)
+console.log(number2)
